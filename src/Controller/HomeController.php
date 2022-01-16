09989fa4d8b7cbb93b8ca0db86a92410ba152881
massidisemi@yahoo.fr <massidisemi@yahoo.fr>
2022-01-16 18:18:53 +0100

    second commit ajust css rout with asset function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/home", name="home")
+ * @Route("/", name="home")
  */
 class HomeController extends AbstractController
 {
