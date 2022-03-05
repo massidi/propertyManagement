7db9d718f01530365ac9ce61d21e9ca53f3e5c38
massidisemi@yahoo.fr <massidisemi@yahoo.fr>
2022-03-05 21:03:24 +0100

    nothing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -23,8 +23,8 @@ class AppFixtures extends Fixture
     {
         $user=new User();
         $user->setEmail('massidisemi@yahoo.fr');
-        $password = $this->hasher->hashPassword($user, 'admin123');
-        $user->setPassword($password);
+//        $password = $this->hasher->hashPassword($user, 'admin123');
+        $user->setPassword("admin123");
         $user->setName("reddy");
         $user->setLastName("massidi");
         $user->setRoles(["ROLE_ADMIN"]);
