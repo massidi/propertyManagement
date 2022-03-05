45892aeca4514488377f73c7530cd5787ccf7e0d
massidisemi@yahoo.fr <massidisemi@yahoo.fr>
2022-03-06 00:09:27 +0100

    nothing

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/client")
+ * @Route("/admin/client")
  */
 class ClientController extends AbstractController
 {
--- src/Controller/FacturationsController.php
+++ src/Controller/FacturationsController.php
@@ -21,7 +21,7 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 
 /**
- * @Route("/facturations")
+ * @Route("/admin/facturations")
  */
 class FacturationsController extends AbstractController
 {
