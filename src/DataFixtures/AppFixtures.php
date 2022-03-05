c59eff15e55b0297edc9586be2d406d477aaac59
massidisemi@yahoo.fr <massidisemi@yahoo.fr>
2022-03-05 23:23:13 +0100

    nothing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -21,13 +21,13 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $user=new User();
-        $user->setEmail("massidisemi@yahoo.fr");
-        $password = $this->hasher->hashPassword($user, "123456789");
-        $user->setPassword($password);
-        $user->setName("reddy");
-        $user->setLastName("massidi");
+        $user= new User();
+        $user->setEmail('massidisemi@yahoo.fr');
+        $password = $this->hasher->hashPassword($user, '123456789');
         $user->setRoles(["ROLE_ADMIN"]);
+        $user->setPassword($password);
+        $user->setName('reddy');
+        $user->setLastName('massidi');
         $manager->persist($user);
 
         $manager->flush();
